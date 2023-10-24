254a3d640851c0f222028d76c26672c131d3cbef
Restyled.io <commits@restyled.io>
2023-10-24 16:55:11 +0000

    Restyled by clang-format

--- src/main/java/org/fluentUserAPI/model/Address.java
+++ src/main/java/org/fluentUserAPI/model/Address.java
@@ -1,17 +1,16 @@
 package org.fluentUserAPI.model;
 
 public final class Address {
-    private int numeroRue;
-    private String nomRue;
-    private String codePostal;
-    private String ville;
+  private int numeroRue;
+  private String nomRue;
+  private String codePostal;
+  private String ville;
 
-    private Address(int numeroRue, String nomRue, String codePostal, String ville) {
-        this.numeroRue = numeroRue;
-        this.nomRue = nomRue;
-        this.codePostal = codePostal;
-        this.ville = ville;
-    }
-
-
+  private Address(int numeroRue, String nomRue, String codePostal,
+                  String ville) {
+    this.numeroRue = numeroRue;
+    this.nomRue = nomRue;
+    this.codePostal = codePostal;
+    this.ville = ville;
+  }
 }
