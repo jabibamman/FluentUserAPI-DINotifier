88d6352863496ce0bc058196925c46611fc48e36
James <james.bidule@gmail.com>
2023-10-24 18:22:31 +0200

    refacto: update user constructor

--- src/main/java/org/fluentUserAPI/model/User.java
+++ src/main/java/org/fluentUserAPI/model/User.java
@@ -2,16 +2,16 @@ package org.fluentUserAPI.model;
 
 import java.util.Objects;
 
-public record User(String firstname, String lastname, int age , int street_number, String street_name, int postal_cod , String city) {
+public record User(String firstname, String lastname, int age ,
+                   int street_number, String street_name,
+                   int postal_code , String city) {
     public User {
         Objects.requireNonNull(firstname);
         Objects.requireNonNull(lastname);
-        if(age<0) {
-            throw new  IllegalArgumentException();
-        }
+        if(age<0) throw new IllegalArgumentException();
         Objects.requireNonNull(street_name);
         Objects.requireNonNull(street_name);
-        Objects.requireNonNull(postal_cod);
+        Objects.requireNonNull(postal_code);
         Objects.requireNonNull(city);
 
     }
