09a74b0dd44e5b712a8d54bd65007f9764f824d8
James <james.bidule@gmail.com>
2023-10-24 17:57:15 +0200

    ✨ Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# FluentUserAPI-DINotifier
---
+++ src/main/java/org/fluentUserAPI/Main.java
@@ -0,0 +1,7 @@
+package org.fluentUserAPI;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
