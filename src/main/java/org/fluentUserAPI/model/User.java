51ae86820dfecae8c7442dc228341aae43240c9e
samira1999 <seddar20191@gmail.com>
2023-10-24 18:40:46 +0200

    .

--- src/main/java/org/fluentUserAPI/model/User.java
+++ src/main/java/org/fluentUserAPI/model/User.java
@@ -2,9 +2,9 @@ package org.fluentUserAPI.model;
 
 import java.util.Objects;
 
-public record User(String firstname, String lastname, int age ,Address adress ) {
+public record User(String firstName, String lastname, int age ,Address adress ) {
     public User {
-        Objects.requireNonNull(firstname);
+        Objects.requireNonNull(firstName);
         Objects.requireNonNull(lastname);
         if(age<0) {
             throw new  IllegalArgumentException();
