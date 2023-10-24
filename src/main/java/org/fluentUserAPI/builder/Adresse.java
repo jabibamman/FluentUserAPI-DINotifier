ce55ac7ee36a510f6478c63c4701a6ab200cfd80
Ronan Kielt <rkielt@myges.fr>
2023-10-24 18:09:13 +0200

    feat: address class

---
+++ src/main/java/org/fluentUserAPI/builder/Adresse.java
@@ -0,0 +1,17 @@
+package org.fluentUserAPI.builder;
+
+final class Adresse {
+    private String numeroRue;
+    private String nomRue;
+    private String codePostal;
+    private String ville;
+
+    private Adresse(String numeroRue, String nomRue, String codePostal, String ville) {
+        this.numeroRue = numeroRue;
+        this.nomRue = nomRue;
+        this.codePostal = codePostal;
+        this.ville = ville;
+    }
+
+
+}
