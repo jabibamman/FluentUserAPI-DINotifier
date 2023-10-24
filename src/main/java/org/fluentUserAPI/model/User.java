99696da3ee581e631dc91ea6b8f490ecb05d324d
James <james.bidule@gmail.com>
2023-10-24 18:54:23 +0200

    fix: update user model

--- src/main/java/org/fluentUserAPI/model/User.java
+++ src/main/java/org/fluentUserAPI/model/User.java
@@ -3,16 +3,12 @@ package org.fluentUserAPI.model;
 import java.util.Objects;
 
 public record User(String firstname, String lastname, int age,
-                   int street_number, String street_name, int postal_code,
-                   String city) {
+                   Address address) {
   public User {
     Objects.requireNonNull(firstname);
     Objects.requireNonNull(lastname);
     if (age < 0)
       throw new IllegalArgumentException();
-    Objects.requireNonNull(street_name);
-    Objects.requireNonNull(street_name);
-    Objects.requireNonNull(postal_code);
-    Objects.requireNonNull(city);
+    Objects.requireNonNull(address);
   }
 }
